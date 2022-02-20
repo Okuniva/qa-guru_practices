70694bf0329049ac55af3c62631ae9772897d41c
Alex Heal <24870446+AlexHeal@users.noreply.github.com>
2022-02-20 15:43:52 +0500

    ADD: lesson 1 completed

---
+++ src/test/java/qa_guru/practices/lesson_1/SearchTests.java
@@ -0,0 +1,16 @@
+package qa_guru.practices.lesson_1;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SearchTests {
+    @Test
+    void selenideSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("selenide.org"));
+    }
+}
