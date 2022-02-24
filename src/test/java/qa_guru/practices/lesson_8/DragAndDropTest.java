8c419dfc0c5c7dc4d73d5ffcc50b0eda8cfff7b9
Alex Heal <24870446+AlexHeal@users.noreply.github.com>
2022-02-24 17:39:42 +0500

    ADD: complete lesson_8

---
+++ src/test/java/qa_guru/practices/lesson_8/DragAndDropTest.java
@@ -0,0 +1,22 @@
+package qa_guru.practices.lesson_8;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DragAndDropTest {
+    @Test
+    void dragAndDropTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $(".example h3").shouldHave(text("Drag and Drop"));
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+}
---
+++ src/test/java/qa_guru/practices/lesson_8/VerifySelenideWikiTest.java
@@ -0,0 +1,42 @@
+package qa_guru.practices.lesson_8;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class VerifySelenideWikiTest {
+    @Test
+    void verifySoftAssertionsTest() {
+        Configuration.browserSize = "1920x1080";
+        open("https://github.com/selenide/selenide");
+        $("h1").shouldHave(text("selenide / selenide"));
+
+        $(by("data-content", "Wiki")).click();
+        $(byText("Welcome to the selenide wiki!")).shouldBe(visible);
+
+        $("#wiki-pages-box").$(withText("more pages")).click();
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $("#wiki-wrapper h1").shouldHave(text("SoftAssertions"));
+        $("#wiki-body").shouldHave(
+                text("JUnit5 extension - "),
+                text("com.codeborne.selenide.junit5.SoftAssertsExtension"),
+                text("Using JUnit5 extend test class:"),
+                text("@ExtendWith({SoftAssertsExtension.class}) " +
+                        "class Tests { " +
+                        "  @Test " +
+                        "  void test() { " +
+                        "    Configuration.assertionMode = SOFT; " +
+                        "    open(\"page.html\"); " +
+                        " " +
+                        "    $(\"#first\").should(visible).click(); " +
+                        "    $(\"#second\").should(visible).click(); " +
+                        "  } " +
+                        "}")
+                );
+    }
+}
