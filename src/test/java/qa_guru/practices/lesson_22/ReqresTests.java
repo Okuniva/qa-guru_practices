907f67b2f3f7a4e83282a7e2e9dea606c6125972
Alex Heal <24870446+AlexHeal@users.noreply.github.com>
2022-04-24 22:45:22 +0500

    ADD: getResourceListTest

--- src/test/java/qa_guru/practices/lesson_22/ReqresTests.java
+++ src/test/java/qa_guru/practices/lesson_22/ReqresTests.java
@@ -2,8 +2,12 @@ package qa_guru.practices.lesson_22;
 
 import io.qameta.allure.*;
 import org.junit.jupiter.api.Test;
+import qa_guru.practices.lesson_22.data.Resource;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.get;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.hamcrest.Matchers.is;
 
 @Feature("Api Tests")
@@ -23,20 +27,17 @@ public class ReqresTests {
                         "data.email", is("janet.weaver@reqres.in")
                 );
     }
-    
+
     @Test
-    @Story("Authorization")
+    @Story("MainProducts")
     @Severity(SeverityLevel.CRITICAL)
-    @Description(
-            "Verify get user 2 have id = 2, email = 'janet.weaver@reqres.in'"
-    )
-    void getSingleUserTest() {
-        get("https://reqres.in/api/users/2")
+    void getResourceListTest() {
+        List<Resource> resources = get("https://reqres.in/api/unknown")
                 .then()
                 .statusCode(200)
-                .body("data.id", is(2),
-                        "data.email", is("janet.weaver@reqres.in")
-                );
+                .extract()
+                .path("data");
+
     }
 
 
---
+++ src/test/java/qa_guru/practices/lesson_22/data/Resource.java
@@ -0,0 +1,49 @@
+package qa_guru.practices.lesson_22.data;
+
+public class Resource {
+    private Integer id;
+    private String name;
+    private Integer year;
+    private String color;
+    private String pantoneValue;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getPantoneValue() {
+        return pantoneValue;
+    }
+
+    public void setPantoneValue(String pantoneValue) {
+        this.pantoneValue = pantoneValue;
+    }
+}
