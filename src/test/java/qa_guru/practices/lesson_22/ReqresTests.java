8c4cf1b77d675ff84a023849ecd37e1411e6cf72
Alex Heal <24870446+AlexHeal@users.noreply.github.com>
2022-04-26 19:47:21 +0500

    ADD: postEmptyPasswordTest

--- src/test/java/qa_guru/practices/lesson_22/ReqresTests.java
+++ src/test/java/qa_guru/practices/lesson_22/ReqresTests.java
@@ -1,17 +1,14 @@
 package qa_guru.practices.lesson_22;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import io.qameta.allure.*;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
-import qa_guru.practices.lesson_22.data.Resource;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static com.codeborne.pdftest.assertj.Assertions.assertThat;
 import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
 @Feature("Api Tests")
@@ -20,12 +17,10 @@ public class ReqresTests {
     @Test
     @Story("Authorization")
     @Severity(SeverityLevel.CRITICAL)
-    @Description(
-            "Verify get user 2 have id = 2, email = 'janet.weaver@reqres.in'"
-    )
+    @Description("Verify get user 2 have id = 2, email = 'janet.weaver@reqres.in'")
     void getSingleUserTest() {
         get("https://reqres.in/api/users/2")
-            .then()
+                .then()
                 .statusCode(200)
                 .body("data.id", is(2),
                         "data.email", is("janet.weaver@reqres.in")
@@ -37,9 +32,9 @@ public class ReqresTests {
     @Severity(SeverityLevel.CRITICAL)
     void getResourceListTest() {
         List<String> resourcesNames = get("https://reqres.in/api/unknown")
-            .then()
+                .then()
                 .statusCode(200)
-            .extract()
+                .extract()
                 .path("data.name");
 
         List<String> expectedNames = List.of("cerulean", "fuchsia rose", "true red", "aqua sky", "tigerlily", "blue turquoise");
@@ -47,6 +42,21 @@ public class ReqresTests {
         assertThat(resourcesNames).isEqualTo(expectedNames);
     }
 
+    @Test
+    @Story("Authorization")
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("post empty password on api/register, return 400 error message")
+    void postEmptyPasswordTest() {
+        String authorizedData = "{\"email\": \"sydney@fife\"}";
 
+        given()
+                .contentType(ContentType.JSON)
+                .body(authorizedData)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .statusCode(400)
+                .body("error", is("Missing password"));
 
+    }
 }
