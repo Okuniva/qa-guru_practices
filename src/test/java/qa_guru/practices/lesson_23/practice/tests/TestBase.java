6cab675ceb2076a17a14fcf62d894ac204cc0070
Alex Heal <24870446+AlexHeal@users.noreply.github.com>
2022-05-04 15:25:26 +0500

    FIX: temp disable video

--- src/test/java/qa_guru/practices/lesson_23/practice/tests/TestBase.java
+++ src/test/java/qa_guru/practices/lesson_23/practice/tests/TestBase.java
@@ -33,7 +33,7 @@ public class TestBase {
         Selenide.closeWebDriver();
 
         if (Project.isVideoOn()) {
-            AllureAttachments.addVideo(sessionId);
+           // AllureAttachments.addVideo(sessionId);
         }
     }
 }
